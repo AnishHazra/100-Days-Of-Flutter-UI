0fca9ff043470b3e77eb0186791235a7ff215e22
AnishHazra <anishhazra108@gmail.com>
2024-03-08 16:36:01 +0530

    starter template

---
+++ fitness_app/lib/fitness_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FitnessHomePage extends StatefulWidget {
+  const FitnessHomePage({super.key});
+
+  @override
+  State<FitnessHomePage> createState() => _FitnessHomePageState();
+}
+
+class _FitnessHomePageState extends State<FitnessHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ fitness_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:fitness_app/fitness_home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        useMaterial3: true,
+      ),
+      home: const FitnessHomePage(),
+    );
+  }
+}
