8b7bc6399366d27a2d9fe238d09cdcce4b9bd3a2
AnishHazra <anishhazra108@gmail.com>
2024-03-13 01:48:11 +0530

    images & description added

---
+++ naruto_characters/lib/anime_characters.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:naruto_characters/model/model.dart';
+
+class AnimeCharacters extends StatefulWidget {
+  const AnimeCharacters({super.key});
+
+  @override
+  State<AnimeCharacters> createState() => _AnimeCharactersState();
+}
+
+class _AnimeCharactersState extends State<AnimeCharacters> {
+  final character = characters.length;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          itemCount: character,
+          itemBuilder: (context, index) {
+            return Stack(
+              children: [
+                //For images
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.network(
+                    characters[index]['image'],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          characters[index]['name'],
+                          style: const TextStyle(
+                            fontSize: 42,
+                            height: 1,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          characters[index]['about'],
+                          style: const TextStyle(
+                            fontSize: 16,
+                            height: 1,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white70,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                // height: 60,
+                                // width: 150,
+                                // decoration: BoxDecoration(
+                                //   color: Colors.blue,
+                                //   borderRadius: BorderRadius.circular(30),
+                                // ),
+                                child: Text(
+                                  characters[index]['topAbilities'],
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
--- naruto_characters/lib/main.dart
+++ naruto_characters/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:naruto_characters/model/anime_characters.dart';
+import 'package:naruto_characters/anime_characters.dart';
 
 void main() {
   runApp(const MyApp());
