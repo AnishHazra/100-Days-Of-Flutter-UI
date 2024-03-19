103f44a58f89bf2759f20c393484b943416467b2
AnishHazra <anishhazra108@gmail.com>
2024-03-19 16:35:51 +0530

    shoes home page done

--- day5_shoes/lib/pages/home_page.dart
+++ day5_shoes/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:day5_shoes/pages/shoes_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -19,20 +20,195 @@ class HomePage extends StatelessWidget {
         ),
         actions: [
           IconButton(
+            onPressed: () {},
             icon: const Icon(
               Icons.notifications_none,
-              color: Colors.black,
             ),
-            onPressed: () {},
           ),
           IconButton(
+            onPressed: () {},
             icon: const Icon(
               Icons.shopping_cart,
-              color: Colors.black,
             ),
-            onPressed: () {},
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 2 / 1,
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "All",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                    AspectRatio(
+                      aspectRatio: 2.2 / 1,
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Sneakers",
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ),
+                      ),
+                    ),
+                    AspectRatio(
+                      aspectRatio: 2.2 / 1,
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Football",
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ),
+                      ),
+                    ),
+                    AspectRatio(
+                      aspectRatio: 2.2 / 1,
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Soccer",
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              makeItem(
+                  image: 'assets/images/five.jpg',
+                  tag: 'red',
+                  context: context),
+              makeItem(
+                  image: 'assets/images/one.jpg',
+                  tag: 'green',
+                  context: context),
+              makeItem(
+                  image: 'assets/images/two.jpg',
+                  tag: 'brown',
+                  context: context),
+              makeItem(
+                  image: 'assets/images/four.jpg',
+                  tag: 'ligthgreen',
+                  context: context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeItem({image, tag, context}) {
+    return Hero(
+      tag: tag,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ShoesPage()),
+          );
+        },
+        child: Container(
+          height: 250,
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.only(bottom: 20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: AssetImage(image),
+              fit: BoxFit.cover,
+            ),
+            boxShadow: const [
+              BoxShadow(
+                color: Color(0xff808080),
+                blurRadius: 10,
+                offset: Offset(0, 10),
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Sneakers",
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Nike",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 35,
+                    height: 35,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.favorite_border,
+                        size: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ day5_shoes/lib/pages/shoes_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoesPage extends StatefulWidget {
+  const ShoesPage({super.key});
+
+  @override
+  State<ShoesPage> createState() => _ShoesPageState();
+}
+
+class _ShoesPageState extends State<ShoesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
